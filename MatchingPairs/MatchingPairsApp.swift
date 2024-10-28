ec2e699462362f7aade2f062fe1b6980e0b25577
razvan-secure <razvan.secure@golfgenius.com>
2024-10-29 00:17:36 +0200

    Initial Commit

---
+++ MatchingPairs/MatchingPairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingPairsApp.swift
+//  MatchingPairs
+//
+//  Created by Razvan Secure on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchingPairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
